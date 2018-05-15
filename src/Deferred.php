048a64e7ba6c1a8b0c8b64812cc8416d60ba38aa
digit <digit.vova@gmail.com>
2018-05-15 12:13:36 +0300

    Deferred fix

--- src/Deferred.php
+++ src/Deferred.php
@@ -2,17 +2,28 @@
 
 namespace Reaction\Promise;
 
+use React\Promise\PromisorInterface;
+
 /**
  * Class Deferred. Created to work with \Reaction\Promise\Promise
  * @package Reaction\Promise
  */
-class Deferred extends \React\Promise\Deferred
+class Deferred implements PromisorInterface
 {
-    private $promise;
-    private $resolveCallback;
-    private $rejectCallback;
-    private $notifyCallback;
-    private $canceller;
+    protected $promise;
+    protected $resolveCallback;
+    protected $rejectCallback;
+    protected $notifyCallback;
+    protected $canceller;
+
+    /**
+     * Deferred constructor.
+     * @param callable|null $canceller
+     */
+    public function __construct(callable $canceller = null)
+    {
+        $this->canceller = $canceller;
+    }
 
     /**
      * Create promise if needed
@@ -29,5 +40,48 @@ class Deferred extends \React\Promise\Deferred
         }
 
         return $this->promise;
+    }
+
+    /**
+     * Resolve deferred promise
+     * @param mixed $value
+     */
+    public function resolve($value = null)
+    {
+        $this->promise();
+
+        call_user_func($this->resolveCallback, $value);
+    }
+
+    /**
+     * Reject deferred promise
+     * @param mixed $reason
+     */
+    public function reject($reason = null)
+    {
+        $this->promise();
+
+        call_user_func($this->rejectCallback, $reason);
+    }
+
+    /**
+     * @deprecated 2.6.0 Progress support is deprecated and should not be used anymore.
+     * @param mixed $update
+     */
+    public function notify($update = null)
+    {
+        $this->promise();
+
+        call_user_func($this->notifyCallback, $update);
+    }
+
+    /**
+     * @deprecated 2.2.0
+     * @see Deferred::notify()
+     * @param mixed $update
+     */
+    public function progress($update = null)
+    {
+        $this->notify($update);
     }
 }
